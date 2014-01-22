9e1fb7812852e858dbb2434e82cf85f9740a8f1c
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-01-22 23:57:54 +0100

    Added "task" tag for Activity. Changed innowork-core depency to version 3.3.

--- source/core/classes/innowork/projects/InnoworkTask.php
+++ source/core/classes/innowork/projects/InnoworkTask.php
@@ -10,6 +10,7 @@ class InnoworkTask extends InnoworkItem
     public $mNoTrash = false;
     public $mConvertible = true;
     public $mNoAcl = true;
+    public $mTags = array('task');
     const ITEM_TYPE = 'task';
 
     //var $mNoAcl = true;
